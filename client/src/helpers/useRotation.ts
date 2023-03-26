816210fef8c22d07f2c7e7cfc93ff042628faf71
nemanjamalesija <malesija13@gmail.com>
2023-03-26 19:59:56 +0200

    Add title. Clean code

--- client/src/helpers/useRotation.ts
+++ client/src/helpers/useRotation.ts
@@ -5,12 +5,7 @@ export const useRotation = () => {
   const [rotation, setRotation] = useState([0, 0, 0]);
 
   useFrame((state, delta) => {
-    // Update the Y-axis rotation based on elapsed time
-    setRotation([
-      0,
-      rotation[1] + delta * 0.8, // adjust the speed as desired
-      0,
-    ]);
+    setRotation([0, rotation[1] + delta * 0.8, 0]);
   });
 
   return { rotation };
--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
   cors: {
-    origin: 'http://127.0.0.1:5174',
+    origin: 'http://127.0.0.1:5173',
     methods: ['GET'],
   },
 });
